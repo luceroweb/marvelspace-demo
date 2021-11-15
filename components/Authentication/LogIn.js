437678ab34f3178c360cf1cfe4c22c02c37cab5d
Juan Lucero <jlucero@alhpaworks.tech>
2021-11-15 09:40:39 -0800

    Added login page to get logged in token from API

--- components/Authentication/LogIn.js
+++ components/Authentication/LogIn.js
@@ -1,26 +1,104 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, TextInput, Button } from "react-native";
 import globalStyles from "../utils/globalStyles";
 import Layout from "../Layout";
 
 export default function LogIn({ navigation }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedin] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(
+    () => {
+      if(loading) {
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'username': `${username}`,
+            'password': `${password}`
+          })
+        };
+        
+        fetch(`https://jualuc1.dreamhosters.com/wp-json/jwt-auth/v1/token`, options)
+        .then(response => response.json())
+        .then(data => {
+          data.token 
+            ? formSuccess(data)
+            : formError(data)
+        })
+      }
+    },
+    [loading]
+  );
+
+	const onFormSubmit = () => {
+    setError('');
+    setLoading(true);
+	};
+
+  const formSuccess = (data) => {
+    setToken(data.token);
+    setLoggedin(true);
+    setLoading(false);
+    setUsername('');
+    setPassword('');
+    navigation.navigate('Newsfeed');
+  }
+
+  const formError = (data) => {
+    const regex = /<[^>]*>/g;
+    setLoading(false);
+    setPassword('');
+    data?.message 
+      ? setError(
+        data.message
+          .replaceAll(regex, "")
+      )
+      :'';
+    console.log(data);
+  }
 	
 	return (
 		<Layout navigation={navigation}>
+      {loggedIn 
+        ? <View>
+          <Text>Logged In</Text>
+        </View>
+        : (
+          <View style={globalStyles.narrowForm}>
+              <Text style={[globalStyles.text, globalStyles.bold]}>Login</Text>
 
-				<View>
-					<Text style={globalStyles.text}>
-						Login
-					</Text>
+              <Text style={globalStyles.text}>Username:</Text>
+              <TextInput
+                style={globalStyles.textInput}
+                value={username}
+                onChangeText={setUsername}
+                onSubmitEditing={onFormSubmit}
+              />
 
-          <View style={globalStyles.menu}>
-            <Text style={globalStyles.menuText} onPress={() => navigation.navigate('SignUp')}>Sign Up</Text>
-            <Text style={globalStyles.menuText}>|</Text>
-            <Text style={globalStyles.menuText} onPress={() => navigation.navigate('PasswordReset')}>Reset Password</Text>
+              <Text style={globalStyles.text}>Password:</Text>
+              <TextInput
+                style={globalStyles.textInput}
+                value={password}
+                onChangeText={setPassword}
+                onSubmitEditing={onFormSubmit}
+                secureTextEntry={true}
+              />
+              <Button
+                onPress={onFormSubmit}
+                title="Login"
+              />
+              <Text>{loading && 'Loading'}</Text>
+              <Text>{error}</Text>
           </View>
-
-				</View>
-        
+        )
+      }
 		</Layout>
 	);
 }
